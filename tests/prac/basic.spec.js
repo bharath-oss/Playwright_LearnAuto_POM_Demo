5a82f92e11457bea567ee0db98339ec8602358d9
bharath-oss <94530687+bharath-oss@users.noreply.github.com>
2025-12-27 14:07:32 +0530

    Delete tests/prac directory

--- tests/prac/basic.spec.js
+++
@@ -1,9 +0,0 @@
-import test, { expect } from "@playwright/test";
-
-test("Basic Playwright Test", async ({ page }) => {
-
-  await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-  await page.waitForTimeout(2000);
-  await page.title().then((title)=>{expect(title).toBe("OrangeHRM")});
-
-});
